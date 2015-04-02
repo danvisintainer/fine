b742971898224cf8de08f5de4939193811a75006
danvisintainer <dan@danvisintainer.com>
2015-04-02 09:06:50 -0400

    major update of matcher, add early index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,27 +10,27 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def go
+  def get_matching_tweets
 
-    r = client.search("\"i feel\"", result_type: "recent").take(200).collect do |t|
+    r = client.search("\"i feel\"", result_type: "recent").take(600).collect do |t|
       "#{t.text}"
     end
 
     r.delete_if do |t|
-      !!(t =~ /(can't feel)|(RT @)|(how i feel)|(what i feel)|(way i feel)|(feel like)|(feel it)|(feel you)|(feel u)|(feel ya)|(feel for)|(feel my)|(feel that)|(feel this)|(feel the)|(feel when)|(feel i)|(feel a)|(feel to)|(should i feel)|(feel\.)/i) ||
+      !!(t =~ /(can't feel)|(RT @)|(how i feel)|(what i feel)|(way i feel)|(feel like)|(feel left)|(feel how)|(feel from)|(feel now)|(feel and)|(feel he)|(feel with)|(feel she)|(feel no)|(feel we)|(feel but)|(feel they)|(feel him)|(feel her)|(feel it)|(feel you)|(feel back)|(feel u)|(feel ya)|(feel for)|(feel my)|(feel our)|(feel their)|(feel out)|(feel that)|(feel this)|(feel the)|(feel self)|(feel when)|(feel than)|(feel i)|(feel a)|(feel to)|(should i feel)|(feel\.)|(feel \.)/i) ||
       !!!(t =~ /feel\b/i)
     end
-
 
     result = {}
 
     r.each do |t|
       puts "Processing: [#{t}]"
       a = t.downcase.split
+      a = a.each {|e| e.gsub(/[^\p{Alnum}\p{Punct}]/, '')}
+      next if !a.include?("feel")
       word_index = a.index("feel") + 1
       
-
-      while !!(a[word_index] =~ /so\b|very\b|really\b|extremely\b|completely\b|as\b|more\b|kinda\b/i) && word_index <= a.length
+      while !!(a[word_index] =~ /so\b|very\b|really\b|rly\b|fucking\b|extremely\b|completely\b|some\b|as\b|more\b|kinda\b|even\b|somewhat\b|way\b|damn\b|fully\b|(a bit)\b/i) && word_index <= a.length
         word_index += 1
       end
 
@@ -43,7 +43,10 @@ class ApplicationController < ActionController::Base
         result[a[word_index]] = result[a[word_index]] + 1
         puts "Adding to '#{a[word_index]}'"
       end
+      
     end
+
+    result
 
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 
   def worldwide
-    
+    @tweets = get_matching_tweets
   end
 end
