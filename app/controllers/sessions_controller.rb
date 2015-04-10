028436af0e373f9ba42b7852061c0520e889f1ff
danvisintainer <dan@danvisintainer.com>
2015-04-10 10:21:28 -0400

    add past-week chart to word show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,18 @@ class SessionsController < ApplicationController
     @today_count = Tweet.where("created_at >= ?", Time.zone.now.beginning_of_day).count
     @positive_count = Tweet.where("created_at >= ?", Time.zone.now.beginning_of_day).where(polarity: "P").count
     @negative_count = Tweet.where("created_at >= ?", Time.zone.now.beginning_of_day).where(polarity: "N").count
-
   end
 
   def show_word
     @client = client
     @word = params["word"]
     @word_list = Tweet.where(feeling: params["word"]).limit(20)
+
+    this_past_week = []
+    for i in 0..6
+      this_past_week << [(Date.today - i).strftime('%a'), Tweet.where("DATE(created_at) = ?", Date.today - i).where(feeling: @word).count]
+    end
+
+    gon.this_past_week = this_past_week
   end
 end
