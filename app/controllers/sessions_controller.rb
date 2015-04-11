b98e1bd85425caaf03f72d525db0face2f3531c3
danvisintainer <dan@danvisintainer.com>
2015-04-11 11:41:33 -0400

    add logic to accept days input as slug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,11 @@ class SessionsController < ApplicationController
 
   def worldwide
     binding.pry
-    gon.tweets = Tweet.group(:feeling).count
+    # gon.tweets = Tweet.group(:feeling).count
+
+    params["days"] = 0 if params["days"].nil?
+
+    gon.tweets = Tweet.where("DATE(created_at) <= ?", Date.today - params["days"]).group(:feeling).count
 
     @today_count = Tweet.where("created_at >= ?", Time.zone.now.beginning_of_day).count
     @positive_count = Tweet.where("created_at >= ?", Time.zone.now.beginning_of_day).where(polarity: "P").count
