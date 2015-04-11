55e2b76a80eb7e7e85b86c978e7861b755828f65
danvisintainer <dan@danvisintainer.com>
2015-04-11 18:54:09 -0400

    add array constructor to polarity graph

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -30,31 +30,50 @@ function showPastWeek(d) {
 }
 
 function showPolarityChart(d) {
-  debugger;
+
+  labels = [];
+  dataPositive = [];
+  dataNegative = [];
+  dataUnknown = [];
+
+  d.forEach(function(e) {
+    labels.push(e.date);
+    dataPositive.push(e.positive);
+    dataNegative.push(e.negative);
+    dataUnknown.push(e.unknown);
+  });
 
   var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: labels,
     datasets: [
         {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
+            label: "positive feelings",
+            fillColor: "rgba(137,177,116,0.5)",
+            strokeColor: "rgba(137,177,116,0.8)",
+            highlightFill: "rgba(137,177,116,0.75)",
+            highlightStroke: "rgba(137,177,116,1)",
+            data: dataPositive
         },
         {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.5)",
-            strokeColor: "rgba(151,187,205,0.8)",
-            highlightFill: "rgba(151,187,205,0.75)",
-            highlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
+            label: "negative feelings",
+            fillColor: "rgba(174,62,61,0.5)",
+            strokeColor: "rgba(174,62,61,0.8)",
+            highlightFill: "rgba(174,62,61,0.75)",
+            highlightStroke: "rgba(174,62,61,1)",
+            data: dataNegative
+        },
+        {
+            label: "unknown feelings",
+            fillColor: "rgba(175,175,175,0.5)",
+            strokeColor: "rgba(175,175,175,0.8)",
+            highlightFill: "rgba(175,175,175,0.75)",
+            highlightStroke: "rgba(175,175,175,1)",
+            data: dataUnknown
         }
       ]
     };
 
     var ctx = document.getElementById("polarity-chart").getContext("2d");
-    var myBarChart = new Chart(ctx).Bar(data, options);
+    var myBarChart = new Chart(ctx).Bar(data);
 
 }
