11984f81999a9b278194108a1518b894c428fa97
danvisintainer <dan@danvisintainer.com>
2015-04-12 14:51:33 -0400

    put modal calls in their own JS file

---
+++ app/assets/javascripts/modalCalls.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $("#tweet-list-table").on('click', 'tr', function(e) {
+    word = $(this).children('td').children('h4').children('a').text();
+
+    $.ajax({
+      url: '/word/' + word,
+      success: function(r){
+        callModalFromCloud(r, word);
+      }});
+    });
+
+});
+
+function callModalFromCloud(body, title) {
+  $('#global-modal').modal({
+    keyboard: true
+  });
+  $('div#tweet-area').empty();
+  $('div#tweet-area').append(body);
+  $('.modal-title').empty();
+  $('.modal-title').append('feeling <b>' + title + '</b>');
+
+
+$('#global-modal').on('shown.bs.modal',function(){
+  $('div#chart-area').empty();
+  $('div#chart-area').append('<canvas id="line-chart" width="700" height="250"></canvas>');
+  showPastWeekChart(gon.this_past_week);
+});
+
+}
+
